d4fffa14857b8f6e24e0609746d22b5e01de6d3f
Mac Signer <macsigner@gmx.ch>
2023-10-10 08:18:38 +0200

    feat: add basic translation text

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 import './src/scss/main.scss'
 import raw from './example.srt?raw';
 
-console.log(raw);
+let subtitles = raw.split('\n\n').map(item => {
+    const arr = item.split('\n')
+    return {
+        index: arr[0],
+        timeframe: arr[1],
+        subtitle: arr.slice(2).join('\n'),
+    };
+})
+
+const input = document.querySelector('#input');
+
+let html = '';
+for (let subtitle of subtitles) {
+    html += `
+    <li class="list-group-item">
+        <ul class="list-group" data-index="${subtitle.index}">
+            <li class="list-group-item list-group-item-dark">${subtitle.index}</li>
+            <li class="list-group-item">${subtitle.timeframe}</li>
+            <li class="list-group-item list-group-item-info preserve-white-space">${subtitle.subtitle}</li>
+        </ul>
+    </li>
+`;
+}
+
+input.innerHTML = `<ul class="list-group list-group-flush">${html}</ul>`;
+
+console.log(subtitles);
