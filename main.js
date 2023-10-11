cab4cc08d51aa9b4aa3666ba6df41ccd9b7c4ded
Mac Signer <macsigner@gmx.ch>
2023-10-11 19:11:17 +0200

    feat: add file upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './src/scss/main.scss'
-import raw from './example.srt?raw';
+import demoFile from './example.srt?raw';
 import {debounce} from './src/js/tools/misc.js';
 
 const deeplApiKeyInput = document.querySelector('#key');
@@ -9,7 +9,6 @@ if (localStorage.getItem('deeplApiKey')) {
 }
 
 deeplApiKeyInput.addEventListener('input', debounce(() => {
-    console.log('change');
     localStorage.setItem('deeplApiKey', deeplApiKeyInput.value);
 }, 500));
 
@@ -32,16 +31,42 @@ form.addEventListener('submit', async e => {
     const json = await resp.json();
 
     render(json, output);
+});
+
+form.addEventListener('input', e => {
+    if (e.target.name === 'srtFile') {
+        let file = e.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                subtitles = parseSrtContent(e.target.result);
+                render(subtitles);
+            }
+            reader.onerror = () => {
+                render({
+                    index: 0,
+                    timeframe: '--',
+                    subtitle: 'Error reading file',
+                })
+            }
+            reader.readAsText(file, "UTF-8");
+        }
+    }
 })
 
-let subtitles = raw.split('\n\n').map(item => {
-    const arr = item.split('\n')
-    return {
-        index: arr[0],
-        timeframe: arr[1],
-        subtitle: arr.slice(2).join('\n'),
-    };
-});
+const parseSrtContent = (srt = demoFile) => {
+    return srt.split('\n\n').map(item => {
+        const arr = item.split('\n')
+        return {
+            index: arr[0],
+            timeframe: arr[1],
+            subtitle: arr.slice(2).join('\n'),
+        };
+    });
+}
+
+let subtitles = parseSrtContent();
 
 const input = document.querySelector('#input');
 const output = document.querySelector('#output');
